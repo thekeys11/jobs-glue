17006f98719f18d48d2304165de35be03edc9c7c
Oscar Gonzalez <oscar.gonzalez.bello@gmail.com>
2025-06-06 08:57:33 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,27 +2,19 @@ provider "aws" {
   region = "us-east-1"
   default_tags {
     tags = {
-      environment      = "dev"
-      application_cc   = "5101960070"
-      application_id   = "2248-000"
-      application      = "2248-000"
-      vice_presidency  = "vpe_mercadeo_canales_y_experiencia_de_cliente"
-      area             = "centro_de_servicio_al_cliente"
-      buss_owner       = "eduardo_del_valle"
-      map-migrated     = "migNY782LEQKE"
-      application_name = "post_call_analytics"
-      application_tech_owner = "efraim_casas"
+      environment            = ""
+      application_cc         = ""
+      application_id         = ""
+      application            = ""
+      vice_presidency        = ""
+      area                   = ""
+      buss_owner             = ""
+      map-migrated           = ""
+      application_name       = ""
+      application_tech_owner = ""
     }
   }
 }
-
-terraform {
-  cloud {
-    organization = "banesco-pa-IaC"
-    workspaces {
-      name = "ban-wktf-dev-data-2257-pca-workload"
-    }
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
